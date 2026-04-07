26db2c5147d4e62207c91b38556ac2846540deb6
Mishka <test@mail.ru>
2026-04-07 15:29:32 +0500

    Разработанный класс выхода пользователя из системы. Климов

--- network/src/main/java/com/example/network/datas/users/UserLogout.java
+++ network/src/main/java/com/example/network/datas/users/UserLogout.java
@@ -4,34 +4,27 @@ import com.example.network.domains.apis.MyAsyncTask;
 import com.example.network.domains.callbacks.MyResponseCallback;
 import com.example.network.domains.common.Settings;
 import com.example.network.domains.models.User;
-import com.google.gson.GsonBuilder;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
-public class UserUpdate extends MyAsyncTask {
-    User user;
-
+public class UserLogout extends MyAsyncTask {
     String token;
 
-    public UserUpdate(User user, String token, MyResponseCallback callback) {
+    public UserLogout(String token, MyResponseCallback callback) {
         super(callback);
-        this.user = user;
         this.token = token;
     }
 
     @Override
     protected String doInBackground(Void... voids) {
-        String rawData = new GsonBuilder().create().toJson(user);
-
         try {
-            Connection.Response response = Jsoup.connect(Settings.URL + "/api/user/update")
+            Connection.Response response = Jsoup.connect(Settings.URL + "/api/user/logout")
                     .ignoreContentType(true)
                     .ignoreHttpErrors(true)
-                    .method(Connection.Method.PUT)
+                    .method(Connection.Method.POST)
                     .header("Content-type", "application/json")
                     .header("token", token)
-                    .requestBody(rawData)
                     .execute();
             return response.statusCode() == 200 ?
                     response.body() :
